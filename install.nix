0f918f43b654fda800ec7879004c3ddfe7b29959
anasouardini <ouardinianas@proton.me>
2025-07-08 18:18:49 +0100

    refactor: disko setup is used by multiple profiles

--- install.nix
+++ install.nix
@@ -3,6 +3,6 @@
 pkgs.writeShellScriptBin "install-nixos" ''
   set -eux
 
-  nix run github:nix-community/disko -- --mode disko ./hosts/i5/disko.nix
-  nixos-install --flake .#my-host --no-root-password --impure
+  nix run github:nix-community/disko -- --mode disko ./disko.nix
+  nixos-install --flake .#minimal --no-root-password --impure
 ''
