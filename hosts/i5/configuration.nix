caceae47410314bb1a259bf71a6631fe8d2273f5
anasouardini <ouardinianas@proton.me>
2025-07-08 18:18:18 +0100

    feat: added swap file

--- hosts/i5/configuration.nix
+++ hosts/i5/configuration.nix
@@ -11,4 +11,17 @@
 
   time.timeZone = "UTC";
   system.stateVersion = "24.05";
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 4096; # Size in MB
+    }
+  ];
+  # ensure the swap file is created
+  systemd.tmpfiles.rules = [
+    "f /swapfile 600 root root 0"
+  ];
+  # if using hibernate
+  boot.kernelParams = [ "resume=/swapfile" ];
 }
