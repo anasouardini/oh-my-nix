247f922a8db6f1e63618106258980ab95ddeb8e9
anasouardini <ouardinianas@proton.me>
2025-07-08 18:19:36 +0100

    style: reamed the repo

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eux
 
-REPO_URL="https://github.com/YOURUSERNAME/nixos-flake"
+REPO_URL="https://github.com/anasouardini/oh-my-nix"
 
 cd /mnt
-git clone "$REPO_URL" nixos-flake
-cd nixos-flake
+git clone "$REPO_URL" oh-my-nix
+cd oh-my-nix
 
 nix develop .#install -c install-nixos
